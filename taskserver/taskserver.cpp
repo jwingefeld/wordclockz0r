6eacd7840f7df5cf120211614af0c31410a90b03
Simon Richter <Simon.Richter@hogyros.de>
2016-06-24 11:06:48 +0200

    Build JSON->Text converter using our JSON parser

--- taskserver/taskserver.cpp
+++ taskserver/taskserver.cpp
@@ -2,6 +2,42 @@
 #include "json_parse.hpp"
 #include "json_lex.hpp"
 
+#include <iostream>
+
+std::string unquote(std::string const &s)
+{
+	if(s.size() < 2)
+		return s;
+
+	if(s[0] == '"' && s[s.size()-1] == '"')
+		return s.substr(1, s.size() - 2);
+
+	return s;
+}
+
+class dump_text :
+	public taskserver::cst::node_const_visitor
+{
+public:
+        virtual void visit(taskserver::cst::false_value const &) { std::cout << "false"; };
+        virtual void visit(taskserver::cst::true_value const &) { std::cout << "true"; };
+        virtual void visit(taskserver::cst::null_value const &) { std::cout << "null"; };
+        virtual void visit(taskserver::cst::object_value const &o) { descend(o._1); }
+        virtual void visit(taskserver::cst::array_value const &) { throw; }
+        virtual void visit(taskserver::cst::number_value const &n) { std::cout << n._1; }
+        virtual void visit(taskserver::cst::string_value const &s) { std::cout << s._1; }
+        virtual void visit(taskserver::cst::object_3_chain const &c) { descend(c._1); descend(c._2); }
+        virtual void visit(taskserver::cst::end_of_object_3 const &) { }
+        virtual void visit(taskserver::cst::array_3_chain const &c) { descend(c._1); descend(c._2); }
+        virtual void visit(taskserver::cst::end_of_array_3 const &) { }
+        virtual void visit(taskserver::cst::json const &j) { descend(j._1); }
+        virtual void visit(taskserver::cst::object const &o) { descend(o._1); descend(o._2); }
+        virtual void visit(taskserver::cst::member const &m) { std::cout << unquote(m._1) << "="; descend(m._2); std::cout << std::endl; }
+        virtual void visit(taskserver::cst::array const &a) { throw; }
+        virtual void visit(taskserver::cst::object_3_elem const &e) { descend(e._1); }
+        virtual void visit(taskserver::cst::array_3_elem const &) { throw; }
+};
+
 int main(int, char **)
 {
 	using namespace taskserver::cst;
@@ -20,5 +56,9 @@ int main(int, char **)
 		}
 		json_lex_destroy(scanner);
 	}
+
+	dump_text dump;
+	dump.visit(*job);
+
 	return 0;
 }
